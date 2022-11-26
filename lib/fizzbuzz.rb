450c3f5292691257aa9bffc3cef0cd344c108156
abodian <ajnbodian@gmail.com>
2022-11-26 14:36:50 +0000

    Added all functionality for specification

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,10 @@
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0 && number % 5 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,27 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+
+  it 'returns "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
+  it 'returns "FizzBuzz" when passed a multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+
+  it 'returns the number when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+
+  it 'returns "Fizz" when passed a multiple of 3' do
+    expect(fizzbuzz(6)).to eq "Fizz"
+  end
+
+  it 'returns "Buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(20)).to eq "Buzz"
   end
 end
